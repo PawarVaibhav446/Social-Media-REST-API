8fb837bdb07a476bb0f793db7ffbc8f9fd73325d
Vaibhav Pawar <pawarvaibhav446@gmail.com>
2024-01-30 12:08:17 +0530

    REST API

---
+++ restful-web-services/src/main/java/com/learnSpringBoot/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,29 @@
+package com.learnSpringBoot.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+// REST - API
+@RestController
+public class HelloWorldController {
+
+	// /hello-world -> "Hello World"
+	
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World Bean");
+	}
+	
+	// "/hello-world/path-variable/Vaibhav"
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
+}
