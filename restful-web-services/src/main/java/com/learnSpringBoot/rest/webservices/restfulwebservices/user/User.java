3641535fd7fc68e729682bc9b678023960478e8d
Vaibhav Pawar <pawarvaibhav446@gmail.com>
2024-02-03 19:16:30 +0530

    Integrated H2 and JPA

--- restful-web-services/src/main/java/com/learnSpringBoot/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/learnSpringBoot/rest/webservices/restfulwebservices/user/User.java
@@ -4,21 +4,31 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
+@Entity(name = "user_details")
 public class User {
-	
+
+	protected User() {
+
+	}
+
+	@Id
+	@GeneratedValue
 	private Integer id;
-	
-	@Size(min=2, message = "Name should have atleast 2 characters")
+
+	@Size(min = 2, message = "Name should have atleast 2 characters")
 	@JsonProperty("user_name")
 	private String name;
-	
+
 	@Past(message = "Birth Date should be in the past")
 	@JsonProperty("birth_date")
 	private LocalDate birthDate;
-	
+
 	public User(Integer id, String name, LocalDate birthDate) {
 		super();
 		this.id = id;
@@ -54,7 +64,5 @@ public class User {
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
 	}
-	
-	
-	
+
 }
